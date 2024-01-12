b1d5a88a90d0946e054b525a93613bf045b5bdbf
dipakpandit55 <147433821+dipakpandit55@users.noreply.github.com>
2024-01-12 11:43:09 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
-resource "aws_key_pair" "autodeploy" {
+# Setting up VPC
+//network.tf
+resource "aws_vpc" "blueteam" {
+  cidr_block = "10.0.0.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  }
+# Setting up the subnets
+//subnets.tf
+resource "aws_subnet" "subnet-uno" {
+  cidr_block = "${cidrsubnet(aws_vpc.blueteam.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.blueteam.id}"
+  availability_zone = "us-east-1a"
+}resource "aws_key_pair" "autodeploy" {
   public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 }
 
